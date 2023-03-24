11fbda1029a3dd3a311aee174123902b6f878385
JervyUmandap <jervygu@gmail.com>
2023-03-24 12:34:51 +0800

    pasted project

---
+++ IntroRxSwift/IntroRxSwift/ViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ViewController.swift
+//  IntroRxSwift
+//
+//  Created by Jeff Umandap on 3/23/23.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+struct Product {
+    let imageName: String
+    let title: String
+}
+
+struct ProductViewModel {
+    var items = PublishSubject<[Product]>()
+    
+    func fetchItems() {
+        let products = [
+            Product(imageName: "house", title: "Home"),
+            Product(imageName: "gear", title: "Settings"),
+            Product(imageName: "person.circle", title: "Profile"),
+            Product(imageName: "airplane", title: "Flights"),
+            Product(imageName: "bell", title: "Activity"),
+        ]
+        
+        items.onNext(products)
+        items.onCompleted()
+    }
+}
+
+class ViewController: UIViewController {
+    
+    private let tableView: UITableView = {
+        let table = UITableView(frame: .zero, style: .insetGrouped)
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    private var viewModel = ProductViewModel()
+    
+    private var bag = DisposeBag()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(tableView)
+        tableView.frame = view.bounds
+        bindTableData()
+    }
+    
+    private func bindTableData() {
+        // bind items to table
+        viewModel.items.bind(to: tableView.rx.items(cellIdentifier: "cell",
+                                                    cellType: UITableViewCell.self)) { row, model, cell in
+            cell.textLabel?.text = model.title
+            cell.imageView?.image = UIImage(systemName: model.imageName)
+        }.disposed(by: bag)
+        
+        // bind model selected handler
+        tableView.rx.modelSelected(Product.self).bind { product in
+            
+            print(product.title)
+        }.disposed(by: bag)
+        
+        
+        // fetch items
+        viewModel.fetchItems()
+        
+    }
+
+
+}
+
